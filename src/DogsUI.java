39053ca459078b59cdd2ecb318016355b11d3a79
Lucas Ilstedt <lucasilstedt@gmail.com>
2022-01-06 14:58:10 +0100

    DogsUI 0.2, updated structuring

--- src/DogsUI.java
+++ src/DogsUI.java
@@ -5,75 +5,104 @@ import java.util.ArrayList;
  * @author Lucas Ilstedt, luil7872
  */
 public class DogsUI {
-    private static DogsUI dogsUI = new DogsUI();
     private Reader reader = new Reader();
     private ArrayList<Dog> dogs = new ArrayList<>();
     private ArrayList<Owner> owners = new ArrayList<>();
     
     public static void main(String[] args) {
-        dogsUI.startUp();
-        dogsUI.commandLoop();
-        dogsUI.terminate();
+        DogsUI dogsUI = new DogsUI();
+        dogsUI.run();
+    }
+
+    private void run() {
+        startUp();
+        commandLoop();
+        terminate();
     }
 
     private void startUp() {
-        // intro text
+        System.out.println("Välkommen till DogDaycare's användargränssnitt!");
+        System.out.println("");
+        printAvailableCommands();
+    }
+
+    private void printAvailableCommands() {
+        System.out.println("Följande kommandon finns tillgängliga, ange ett kommando med dess hela namn eller förkortningen inom parentesen:");
+        System.out.println("Lagg till tester (ltt)");
+        System.out.println("Registrera ny hund (rnh)");
+        System.out.println("Lista hundar (lh)");
+        System.out.println("Oka alder (oa)");
+        System.out.println("Ta bort hund (tbh)");
+        System.out.println("Registrera ny agare (rna)");
+        System.out.println("Ge bort hund (gbh)");
+        System.out.println("Lista agare (la)");
+        System.out.println("Ta bort hund fran agare (tbhfa)");
+        System.out.println("Ta bort agare (tba)");
+        System.out.println("Avsluta (a)");
     }
 
     private void commandLoop() {
         String command;
         do {
-            command = reader.readString("Ange kommando", false).toLowerCase();
-        
-            switch (command) {
-                case "at", "add tests":
+            command = readCommand();
+            handleCommand(command);
+        } while (!command.equals("exit") && !command.equals("e") && !command.equals("avsluta") && !command.equals("a"));
+    }
 
-                    break;
+    private String readCommand() {
+        return reader.readString("Ange kommando", false).toLowerCase();
+    }
+
+    private void handleCommand(String command) {
+        switch (command) {
+            case "at", "add tests", "ltt", "lagg till tester":
                 
-                case "rnd", "register new dog":
-                    // Call method(s).
-                    break;
+                break;
             
-                case "ld", "list dogs":
+            case "rnd", "register new dog", "rnh", "registrera ny hund":
+                // Call method(s).
+                break;
+        
+            case "ld", "list dogs", "lh", "lista hundar":
 
-                    break;
+                break;
 
-                case "ia", "increase age":
+            case "ia", "increase age", "oa", "oka alder":
 
-                    break;
+                break;
 
-                case "rd", "remove dog":
+            case "rd", "remove dog", "tbh", "ta bort hund":
 
-                    break;
+                break;
 
-                case "rno", "register new owner":
+            case "rno", "register new owner", "rna", "registrera ny agare":
 
-                    break;
+                break;
 
-                case "gd", "give dog": 
+            case "gd", "give dog", "gbh", "ge bort hund": 
 
-                    break;
+                break;
 
-                case "lo", "list owners":
+            case "lo", "list owners", "la", "lista agare":
 
-                    break;
+                break;
 
-                case "rod", "remove owned dog":
+            case "rod", "remove owned dog", "tbhfa", "ta bort hund fran agare":
 
-                    break;
+                break;
 
-                case "ro", "remove owner":
+            case "ro", "remove owner", "tba", "ta bort agare":
 
-                    break;
+                break;
 
-                case "e", "exit":
-                    break;
+            case "e", "exit", "a", "avsluta":
+                break;
 
-                default:
-                    System.out.println("Error: Unknown command");
-                    break;
-            }
-        } while (!command.equals("exit") && !command.equals("e"));
+            default:
+                System.out.println("Error: Unknown command");
+                printAvailableCommands();
+                break;
+        }    
     }
 
     private void terminate() {
